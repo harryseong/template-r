4ebe4e411713ef92df2753413ddd22dae5f402af
Harry Seong <harryseong@gmail.com>
2023-07-29 18:25:38 -0500

    Updated places API

--- src/common/api/places.ts
+++ src/common/api/places.ts
@@ -2,17 +2,14 @@ import axios, { AxiosResponse } from "axios";
 
 export const PlacesAPI = {
     async getAll(): Promise<AxiosResponse> {
-        const response = await axios
-            .get(
-                `${process.env.REACT_APP_V1_API_ENDPOINT}/places`,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                        'x-api-key': process.env.REACT_APP_API_KEY
-                    }
-                }
-            );
+        const url = `${process.env.REACT_APP_V1_API_ENDPOINT}/places`;
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'x-api-key': process.env.REACT_APP_API_KEY
+        };
+
+        const response = await axios.get(url, { headers });
         return response;
     }
 }
