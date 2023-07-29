7de6b13ff9bd57422b37671caa8c74800b25d667
Harry Seong <harryseong@gmail.com>
2023-07-29 18:25:29 -0500

    Update Places render

--- src/features/places/Places.tsx
+++ src/features/places/Places.tsx
@@ -6,7 +6,7 @@ import { Place, fetchPlacesThunk } from "./placesSlice";
 function Places() {
 
     const placesState = useAppSelector((state: RootState) => state.places);
-    const places = useAppSelector((state: RootState) => state.places.value.places);
+    const places = [...placesState.value.places];
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -18,14 +18,27 @@ function Places() {
         return () => {
             // Cleanup code run when component is removed from page:
         }
-    });
+    }, [dispatch, placesState]);
+
+    const renderPlaces = () => {
+        return (
+            <>
+                {places.map((place: Place) =>
+                    <div key={place.place_id}>
+                        <div>{place.fullName}</div>
+                        <div>{place.description}</div>
+                        <div>{place.years.start} - {place.years.end}</div>
+                        <div>{place.coords.lat}, {place.coords.lng}</div>
+                    </div>)
+                }
+            </>
+        );
+    }
 
     return (
         <div>
             <h1>THIS IS PLACES</h1>
-            <>
-                {places.map((place: Place) => <div key={place.place_id}>{place.fullName}</div>)}
-            </>
+            {renderPlaces()}
         </div>
     );
 }
